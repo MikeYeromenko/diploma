5955bcb33444908b6901596494ed86f2933ac299
Mike <inboxema2015@gmail.com>
2020-07-04 09:00:36 +0300

    Configures translation, adds locale middleware

--- cinema/settings.py
+++ cinema/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'    #'en-us'
 
 TIME_ZONE = 'Europe/Kiev'              # start value 'UTC'
 
